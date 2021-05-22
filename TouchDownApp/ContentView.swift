4bc6deb84a63c1a6f7e9b2b03f12ce2cfba509ec
mahmoud-hajar <mahmoudhagar12@yahoo.com>
2021-05-22 21:04:40 +0200

    setup project

---
+++ TouchDownApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouchDownApp
+//
+//  Created by Ghoost on 5/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchDownApp/TouchDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownAppApp.swift
+//  TouchDownApp
+//
+//  Created by Ghoost on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
